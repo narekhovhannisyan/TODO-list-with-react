3010d31ff9160ae2d7e0a004f8fb14d398fe45ec
narekhovhannisyan <narekgilmour@gmail.com>
2018-03-02 14:58:34 +0400

    components: add SubmitButton and RemoveButton buttons.js

---
+++ src/components/buttons.js
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+export class SubmitButton extends Component {
+  render () {
+    return (
+      <button onClick={() => this.props.onClick(this.props.elemIndex)}>
+        Done
+      </button>
+    )
+  }
+}
+
+export class RemoveButton extends Component {
+  render () {
+    return <button onClick={() => this.props.onClick(this.props.elemIndex)}>Remove</button>
+  }
+}
