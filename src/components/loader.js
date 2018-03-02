496645762de3f8c00d3a3cd7bcf1cafaae877d51
narekhovhannisyan <narekgilmour@gmail.com>
2018-03-02 14:59:28 +0400

    components: add Loader and Dot components to loader.js

---
+++ src/components/loader.js
@@ -0,0 +1,46 @@
+import { Component } from 'react'
+
+export class Loader extends Component {
+  constructor () {
+    super()
+    this.state = {
+      timer: 0
+    }
+  }
+
+  componentDidMount () {
+    this.setState({
+      ...this.state,
+      timer: setInterval(this.props.incrementist, 1000)
+    })
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.state.timer)
+  }
+
+  printingLoop = (count, whatToPrint) => {
+    const array = []
+
+    for (let i = 0; i <= count; i++) {
+      array.push(whatToPrint())
+    }
+
+    return array
+  }
+
+  render () {
+    return (
+      <div>
+        <h1 style={{
+          display: 'inline'
+        }}>Loading</h1>
+        {this.printingLoop(this.props.loadCount, Dot)}
+      </div>
+    )
+  }
+}
+
+const Dot = () => {
+  return (<h1 style={{ display: 'inline' }}>.</h1>)
+}
